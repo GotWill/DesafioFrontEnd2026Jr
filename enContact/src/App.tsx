14629774b0efa74dbd6411381a5950a9c3e7d108
Will <willianpereira0912@gmail.com>
2026-04-23 09:29:10 -0300

    feat: setup react-query and create route home

--- enContact/src/App.tsx
+++ enContact/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/login";
+import Home from "./pages/home";
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </>
--- enContact/src/main.tsx
+++ enContact/src/main.tsx
@@ -5,14 +5,19 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/user.tsx";
 import { Toaster } from "./components/ui/sonner.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-        <Toaster richColors position="bottom-right"/>
-      </UserProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <UserProvider>
+          <App />
+          <Toaster richColors position="bottom-right" />
+        </UserProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 );
